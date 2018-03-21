9eedc60631d141d789320b6dfcd6846362008e50
vsdutka <vsdutka@gmail.com>
2018-03-21 17:52:34 +0300

    run.sh

--- run.sh
+++ run.sh
@@ -1,11 +1,12 @@
 rm -R $HOME/assets/tst$2
 mkdir -p  $HOME/assets/tst$2/wwwroot
 mkdir -p  $HOME/assets/tst$2/log
-cp -a /root/assets/mastercopy/* $HOME/assets/tst$2/wwwroot
+cp -a /root/assets/trunk/wwwroot/* $HOME/assets/tst$2/wwwroot
 chmod -R 777 $HOME/assets/tst$2/log
 
-docker run --rm -p 25111:25111 -p 25112:25112 \
+docker run --rm -p 25$21:25111 -p 25$22:25112 \
 -v $HOME/assets/tst$2/log:/log \
+-v $HOME/assets/trunk/apex5:/apex5 \
 -v $HOME/assets/tst$2/wwwroot:/wwwroot \
 -v $HOME/assets/tst$2/wwwroot/rolf:/ext/tst$2 \
 --name=ipls_tst$2 iplsgo \
